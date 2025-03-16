2df06780a7f154ca8e24618dd60692ef3684087e
DESKTOP-O40GF1D\wahyu <wahyukusumo128@gmail.com>
2025-03-16 19:43:48 +0700

    dasdsa

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'dashboard']);
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::resource('barang', BarangController::class);
-    Route::resource('pinjam_barang', PinjamBarangController::class);
-    Route::get('/pinjam_barang/{id}/qrcode', [PinjamBarangController::class, 'generateQRCode'])->name('pinjam_barang.qrcode');
+Route::get('/', [AuthController::class, 'dashboard']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-});
+Route::resource('barang', BarangController::class);
+Route::resource('pinjam_barang', PinjamBarangController::class);
+Route::get('/pinjam_barang/{id}/qrcode', [PinjamBarangController::class, 'generateQRCode'])->name('pinjam_barang.qrcode');
